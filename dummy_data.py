eb5d9b22b884c346f3be51ce5af54ad1be720615
Lewis Rodgers <lewis.rodgers@cloudsherpas.com>
2016-01-17 11:12:35 -0500

    Refactor

--- dummy_data.py
+++ dummy_data.py
@@ -1,42 +1,49 @@
-""" Generate random copy that looks more 'real world' than lorem ipsum text
+""" Generate random, non latin, copy that looks more 'real world'
 
 https://github.com/jsvine/markovify - Markovify is a simple, extensible Markov chain generator. Right now, its main use is for building Markov models of large corpora of text, and generating random sentences from that.
 """
 import markovify
-        """Place seed text in corpus.txt file
+class BodyCopy(object):
+    def __init__(self, text):
+        self.markovify = markovify.Text(text)
 
-        Tip:
-            Include words related to the project sponsor's industry. More text means more variety in final output.
-        """
-
-class BodyCopy(markovify.Text):
-    def generate_paragraph(self, para_count=1, sen_count=1):
-        """Return a list of paragraphs
-
-        Args:
-            char_limit (int): limit amount of characters in a sentence
-            paragraph_count (int): amount of paragraphs to create
-            sentence_count (int): amount of sentences in a paragraph
-        """
-        results = []
-        for i in xrange(para_count):
-            results.append(
-                ' '.join(self.generate_sentence(sen_count))
-            )
-        return results
-
-    def generate_sentence(self, sen_count=1):
+    def make_sentences(self, sentence_count=1):
         """Return a list of sentences
 
         Args:
-            char_limit (int): limit amount of characters in a sentence
-            sentence_count (int): amount of sentences
+        char_limit (int): limit amount of characters in a sentence
+        sentence_count (int): amount of sentences
         """
         results = []
-        for i in xrange(sen_count):
-            results.append(self.make_sentence())
+        for _ in xrange(sentence_count):
+            results.append(self.markovify.make_sentence())
         return results
-""" Generate names
+
+    def make_paragraphs(self, paragraph_count=1, sentence_count=1):
+        """Return a list of paragraphs
+
+        Args:
+        char_limit (int): limit amount of characters in a sentence
+        paragraph_count (int): amount of paragraphs to create
+        sentence_count (int): amount of sentences in a paragraph
+        """
+        results = []
+        for _ in xrange(paragraph_count):
+            results.append(
+                ' '.join(self.make_sentences(sentence_count))
+            )
+        return results
+
+
+"""Generate names
 
 uinames.com - A simple tool to generate names for use in designs and mockups. Check the documentation for additional parameters.
 """
+import urllib2
+import json
+class UserName:
+    def __init__(self, amount=1, **kwargs):
+        self.uri = 'http://api.uinames.com/?%s' % amount
+
+    def make_names(self):
+        return json.load(urllib2.urlopen(self.uri))
--- test_basic.py
+++ test_basic.py
@@ -1,19 +1,22 @@
 import unittest
+import markovify
 from dummy_data import BodyCopy
 
 
 class BodyCopyTest(unittest.TestCase):
 
     def setUp(self):
-        self.text_model = BodyCopy()
-
-    def test_make_paragraphs(self):
-        para = self.text_model.make_paragraphs(paragraph_count=2)
-        assert(len(para) == 2)
+        with open('corpus.txt') as f:
+            text = f.read()
+        self.model = BodyCopy(text)
 
     def test_make_sentences(self):
-        sen = self.text_model._make_sentences(sentence_count=2)
+        sen = self.model.make_sentences(sentence_count=2)
         assert(len(sen) == 2)
+
+    def test_make_paragraphs(self):
+        para = self.model.make_paragraphs(paragraph_count=2)
+        assert(len(para) == 2)
 
 
 if __name__ == '__main__':
